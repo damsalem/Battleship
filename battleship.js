1e3f201e09e29d6b9aeca8d99aa58a81672b189a
damsalem <drummingdan@gmail.com>
2021-02-08 00:15:44 -0500

    Resizing to Avoid Scrolling

--- battleship.js
+++ battleship.js
@@ -104,17 +104,3 @@ function handleFire(e) {
   controller.processGuess(guess);
   guessInput.value = "";
 }
-
-/* controller.processGuess("A0");
-
-controller.processGuess("A6");
-controller.processGuess("B6");
-controller.processGuess("C6");
-
-controller.processGuess("C4");
-controller.processGuess("D4");
-controller.processGuess("E4");
-
-controller.processGuess("B0");
-controller.processGuess("B1");
-controller.processGuess("B2"); */
